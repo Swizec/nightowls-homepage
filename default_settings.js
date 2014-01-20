0f03dd0d6a3b3d7ecf16aa27979b2cc8bf5f9a8f
Swizec Teller <swizec@swizec.com>
2014-01-20 12:03:48 -0800

    getting ready to add leanpub

---
+++ default_settings.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    leanpub_key: "api key",
+    leanpub_slug: "nightowls"
+};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@
  * GET home page.
  */
 
+var Leanpub = require('leanpub-client'),
+    settings = require('../settings');
+
+
+
 exports.index = function(req, res){
   res.render('index', {  });
 };
