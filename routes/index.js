3058ef804d49b886967a4307241ad55ba5e71915
Swizec Teller <swizec@swizec.com>
2014-01-20 14:12:36 -0800

    showing number of readers

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@
  */
 
 var Leanpub = require('leanpub-client'),
-    settings = require('../settings');
-
-
+    settings = require('../settings'),
+    leanpub = new Leanpub(settings.leanpub_key);
 
 exports.index = function(req, res){
-  res.render('index', {  });
+    leanpub.sales({slug: settings.leanpub_slug, report: 'summary'},
+                  function (err, summary) {
+                      console.log(summary);
+                      res.render('index', 
+                                 {happy_readers: summary.num_happy_readers});
+                  });
 };
