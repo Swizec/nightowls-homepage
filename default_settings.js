de1374714bc72b27a7034a660073c893d7906f50
Swizec Teller <swizec@swizec.com>
2014-01-20 15:44:10 -0800

    simplistic caching

--- default_settings.js
+++ default_settings.js
@@ -1,5 +1,6 @@
 
 module.exports = {
     leanpub_key: "api key",
-    leanpub_slug: "nightowls"
+    leanpub_slug: "nightowls",
+    cache_timeout: 60*60*1000 // 1h
 };
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,24 @@ var Leanpub = require('leanpub-client'),
     settings = require('../settings'),
     leanpub = new Leanpub(settings.leanpub_key);
 
+var cache = {};
+
 exports.index = function(req, res){
-    leanpub.sales({slug: settings.leanpub_slug, report: 'summary'},
-                  function (err, summary) {
-                      console.log(summary);
-                      res.render('index', 
-                                 {happy_readers: summary.num_happy_readers});
-                  });
+    var respond = function (err, summary) {
+        res.render('index',
+                   {happy_readers: summary.num_happy_readers});
+    };
+
+
+    if (!cache.time || Date.now()-cache.time > settings.cache_timeout) {
+        leanpub.sales({slug: settings.leanpub_slug, report: 'summary'},
+                      function (err, summary) {
+                          cache.time = Date.now();
+                          cache.summary = summary;
+
+                          respond(null, summary);
+                      });
+    }else{
+        respond(null, cache.summary);
+    }
 };
