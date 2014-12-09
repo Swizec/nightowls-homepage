e738bbdde0cc11535fa3aa02a9bbb516f9f1cfcc
Swizec Teller <swizec@swizec.com>
2014-12-09 01:12:27 +0100

    allow envvars to handle secrets

---
+++ settings.js
@@ -0,0 +1,14 @@
+
+var settings = {};
+
+try {
+    settings = require("./secret_settings");
+}catch (e) {
+    settings = {};
+}
+
+settings.cache_timeout = 60*60*1000;
+settings.leanpub_key = settings.leanpub_key || process.env.LEANPUB_KEY;
+settings.leanpub_slug = settings.leanpub_slug || process.env.LEANPUB_SLUG;
+
+module.exports = settings;
