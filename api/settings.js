a66b1b6d63cc16ec6253c8cbe0428298357dbc04
Swizec Teller <swizec@swizec.com>
2024-06-20 19:00:18 +0200

    Vercel wants everything in /api

--- api/settings.js
+++ api/settings.js
@@ -1,13 +1,12 @@
-
 var settings = {};
 
 try {
     settings = require("./secret_settings");
-}catch (e) {
+} catch (e) {
     settings = {};
 }
 
-settings.cache_timeout = 60*60*1000;
+settings.cache_timeout = 60 * 60 * 1000;
 settings.leanpub_key = settings.leanpub_key || process.env.LEANPUB_KEY;
 settings.leanpub_slug = settings.leanpub_slug || process.env.LEANPUB_SLUG;
 
